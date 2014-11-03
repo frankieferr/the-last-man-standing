87caf1fd07b8a11dabb81359515e04b36eca16c5
frankieferr <frankstar_frankilicious@hotmail.com>
2014-11-03 21:25:50 +1000

    Added a message for Thomas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def i_hate_thomas
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TheLastManStanding::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get 'the_truth' => 'home#i_hate_thomas'
   # You can have the root of your site routed with "root"
   root 'home#index'
 
