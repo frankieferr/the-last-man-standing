ac0be9cf12c2e6f23f72c009fb539558264950d3
frankieferr <frankstar_frankilicious@hotmail.com>
2014-11-03 20:27:11 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheLastManStanding::Application.config.secret_key_base = 'de8d55c991ad25e6027475cf6ded2b2ac271d687e7dea42f29c136b7cc72505bb529ae9a7796664da8755788b97aa30a3d7b37eb7f989acbda8e8af903271884'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheLastManStanding::Application.config.session_store :cookie_store, key: '_the-last-man-standing_session'
